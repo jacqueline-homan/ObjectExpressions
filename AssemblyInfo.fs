0f468d76310533b36ef69acb0c0db299045f90e2
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-03-28 12:56:52 -0400

    first commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module ObjectExpressiona.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("ObjectExpressiona")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("Codecatenation, LLC")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Program.fs
@@ -0,0 +1,30 @@
+﻿let hiObject = { new obj() with member x.ToString() = "Hi!" }
+
+printfn "%A" hiObject
+
+type IDeepThought =
+    abstract member TheAnswer: int with get
+    abstract member AnswerString: unit -> string
+
+type DeepThought()=
+    interface IDeepThought with
+        member x.TheAnswer = 42
+        member x.AnswerString() = sprintf "The Answer is %d" (x :> IDeepThought).TheAnswer
+
+let htmlDeepThought =
+    let deepThought = DeepThought() :> IDeepThought
+    { new IDeepThought with
+        member x.TheAnswer = deepThought.TheAnswer
+        member x.AnswerString() = sprintf "<b>%s</b>" (deepThought.AnswerString()) }
+
+printfn "%A" (htmlDeepThought.AnswerString())
+
+let confusedDeepThought answer =
+    { new IDeepThought with
+        member x.TheAnswer = answer
+        member x.AnswerString() = "Duh...." }
+
+let cdt = confusedDeepThought 35
+
+printfn "%A" cdt.TheAnswer
+printfn "%A" (cdt.AnswerString()) 
